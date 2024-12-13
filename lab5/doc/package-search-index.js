0b8b0babc360b8bd845f5edd721b2bcb1302a7d7
Никита <nekitos1011@gmail.com>
2024-12-14 01:08:03 +0300

    Удален javadoc

--- lab5/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.controller"},{"l":"com.example.model"},{"l":"com.example.util"},{"l":"com.example.view"}];updateSearchResults();
